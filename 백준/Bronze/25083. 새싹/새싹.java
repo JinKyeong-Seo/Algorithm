9708f3e83c11954378fdad4d51f0f8ed30519014
JinKyeong-Seo <103301658+JinKyeong-Seo@users.noreply.github.com>
2024-02-06 19:25:56 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		String s= "         ,r'\"7\n"+
+				  "r`-_   ,'  ,/\n" +
+				  " \\. \". L_r'\n"+
+				  "   `~\\/\n"+
+				  "      |\n"+
+				  "      |\n";
+		System.out.println(s);
+	}
+}
