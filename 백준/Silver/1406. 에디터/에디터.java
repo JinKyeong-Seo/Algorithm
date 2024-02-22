0086fb9ecea55fe218437fc285634eb585ed0ddf
JinKyeong-Seo <103301658+JinKyeong-Seo@users.noreply.github.com>
2024-02-22 13:48:34 +0900

    [Silver II] Title: 에디터, Time: 492 ms, Memory: 79148 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,130 +1,49 @@
 import java.io.*;
 import java.util.*;
 
-class Node{
-    char data;
-    Node left;
-    Node right;
-    Node(){}
-    Node(char data){
-        this.data = data;
-    }
-}
-class LinkedList{
-    Node head;
-    Node tail;
-    int size;
-    Node pointNode;
-
-    LinkedList(){
-        head = new Node();
-        tail = new Node();
-
-        pointNode = tail;
-
-        head.right = tail;
-        tail.left = head;
-    }
-    void add(char data){
-        Node node = new Node(data);
-        
-        Node curr = tail.left;
-        Node next = tail;
-        
-        node.left = curr;
-        node.right = next;
-        curr.right = node;
-        next.left = node;
-
-        size++;
-    }
-    void moveLeft(){ //pointNode는 head.left를 넘어가지 않음
-        if(pointNode.left == head) return;
-        pointNode = pointNode.left;
-    }
-    void moveRight(){ //pointNode는 tail을 넘어가지 않음
-        if (pointNode == tail) return;
-        pointNode = pointNode.right;
-    }
-
-    void deleteLeft(){
-        if(pointNode.left == head) return;
-        Node curr = pointNode.left.left;
-        Node next = pointNode;
-
-        curr.right = next;
-        next.left = curr;
-        
-        size--;
-    }
-
-    void insert(char data){
-        Node node = new Node(data);
-        
-        Node curr = pointNode.left;
-        Node next = pointNode;
-
-        curr.right = node;
-        next.left = node;
-
-        node.right = next;
-        node.left = curr;
-
-        //if(pointNode.left == head) moveLeft(); // 맨 앞 pointNode에서 왼쪽에 추가해줬으면 pointNode 맨 앞으로 바꿔줘야 함
-         
-        size++;
-    }
-
-    void printList(){
-        Node curr = head.right;
-        StringBuilder sb = new StringBuilder();
-        while(curr.right != null){
-            sb.append(curr.data);
-            curr = curr.right;
-        }
-        System.out.println(sb);
-    }
-}
-
 public class Main {
-    static int N;
-    static LinkedList list;
-    static StringTokenizer st;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader br;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		String text = br.readLine();
+		int M = Integer.parseInt(br.readLine());
+		Stack<Character> left = new Stack<>();
+		Stack<Character> right = new Stack<>();
+		
+		for(int i=0; i<text.length(); i++) {
+			left.push(text.charAt(i));
+		}
+		
+		for(int m=0; m<M; m++) {
+			String now = br.readLine();
+			if(now.equals("L")) {
+				if (!left.isEmpty()) {
+					right.push(left.pop());
+				}
+			} else if (now.equals("D")) {
+				if (!right.isEmpty()) {
+					left.push(right.pop());
+				}
+			} else if (now.equals("B")) {
+				if (!left.isEmpty()) {
+					left.pop();
+				}
+			} else {
+				char c =now.charAt(2);
+				left.push(c);
+			}
+		}
+		
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        list = new LinkedList();    
-        getList();
-        N = Integer.parseInt(br.readLine());
-
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-
-            char cmd = st.nextToken().charAt(0);
-            switch (cmd) {
-                case 'L':
-                    list.moveLeft();
-                    break;
-                case 'D':
-                    list.moveRight();
-                    break;
-                case 'B':
-                    list.deleteLeft();
-                    break;
-                case 'P':
-                    char data = st.nextToken().charAt(0);
-                    list.insert(data);
-                    break;
-            }
-        }
-        list.printList();
-    } 
-
-    static void getList() throws NumberFormatException, IOException{
-        char[] cArr = br.readLine().toCharArray();
-
-        for(char c : cArr){
-           list.add(c);
-        }
-    }
+		while(!left.isEmpty()) {
+			right.push(left.pop());
+		}
+		
+		while(!right.isEmpty()) {
+			sb.append(right.pop());
+		}
+		
+		System.out.println(sb);
+	}
 }
