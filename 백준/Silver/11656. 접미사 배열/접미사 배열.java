549a37ea6b59cfff00da35789ba0dce5c2c49459
JinKyeong-Seo <103301658+JinKyeong-Seo@users.noreply.github.com>
2024-08-25 22:09:08 +0900

    [Silver IV] Title: 접미사 배열, Time: 96 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<S.length(); i++){
+            list.add(S.substring(i));
+        }
+
+        Collections.sort(list);
+        for (String s : list) {
+            sb.append(s).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
