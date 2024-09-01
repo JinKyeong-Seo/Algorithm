bb592c922859a2c92555c47443ec81a68984aaa7
JinKyeong-Seo <103301658+JinKyeong-Seo@users.noreply.github.com>
2024-09-01 23:02:17 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,6 @@
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+where skill_code & (SELECT SUM(CODE) 
+                    FROM SKILLCODES 
+                    WHERE CATEGORY LIKE 'Front End')
+ORDER BY ID;
