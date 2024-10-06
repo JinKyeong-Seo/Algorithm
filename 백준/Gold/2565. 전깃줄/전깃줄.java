118a310c67787ec077765c5ba61fb9061e80a605
JinKyeong-Seo <103301658+JinKyeong-Seo@users.noreply.github.com>
2024-10-06 18:05:29 +0900

    [Gold V] Title: 전깃줄, Time: 76 ms, Memory: 11696 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] line = new int[2][501];
+
+        for(int n=0; n<N; n++){
+            st = new StringTokenizer(br.readLine());
+            line[0][Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i=1; i<=500; i++){
+            for(int j=i; j>=0; j--){
+                if(line[0][i]>line[0][j]) line[1][i] = Math.max(line[1][i], line[1][j]+1);
+            }
+            max = Math.max(max, line[1][i]);
+        }
+
+        System.out.println(N-max);
+    }
+}
