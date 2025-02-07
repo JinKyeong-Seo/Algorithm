7b5da9394f392df2f73223bfb12318078f80c175
JinKyeong-Seo <103301658+JinKyeong-Seo@users.noreply.github.com>
2025-02-08 02:40:04 +0900

    [Gold IV] Title: 인구 이동, Time: 620 ms, Memory: 296796 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class Main {
     static BufferedReader br;
     static StringTokenizer st;
-    static int[][] arr, visited;
+    static int[][] arr;
+    static boolean[][] visited;
     static int N, L, R;
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,25 +24,26 @@ public class Main {
 
         int day = 0;
         while(true) {
-            visited = new int[N][N];
-            int cnt = 0;
+            visited = new boolean[N][N];
+            boolean flag = false;
             for(int r=0; r<N; r++){
                 for(int c=0; c<N; c++){
-                    if(visited[r][c] == 0) {
-                        visited[r][c] = ++cnt;
-                        movePeople(r, c, cnt);
+                    if(!visited[r][c]) {
+                        visited[r][c] = true;
+                        boolean res = movePeople(r, c);
+                        if(res) flag = true;
                     }
                 }
             }
 
-            if(cnt == N*N) break;
+            if(!flag) break;
             day++;
         }
 
         System.out.println(day);
     }
 
-    public static void movePeople(int sr, int sc, int num) {
+    public static boolean movePeople(int sr, int sc) {
         int[] dr = {1, 0, -1, 0};
         int[] dc = {0, 1, 0, -1};
 
@@ -50,16 +52,21 @@ public class Main {
 
         int sum = arr[sr][sc];
         int cnt = 1;
+
+        List<int[]> country = new ArrayList<>();
+        country.add(new int[]{sr, sc});
+
         while(!queue.isEmpty()){
             int[] now = queue.poll();
             for(int d=0; d<4; d++){
                 int nr = now[0] + dr[d];
                 int nc = now[1] + dc[d];
-                if(nr>=0 && nr<N && nc>=0 && nc<N && visited[nr][nc] == 0) {
+                if(nr>=0 && nr<N && nc>=0 && nc<N && !visited[nr][nc]) {
                     int tmp = Math.abs(arr[now[0]][now[1]]-arr[nr][nc]);
                     if(tmp < L || tmp > R) continue;
                     queue.add(new int[]{nr, nc});
-                    visited[nr][nc] = num;
+                    country.add(new int[]{nr, nc});
+                    visited[nr][nc] = true;
                     sum += arr[nr][nc];
                     cnt++;
                 }
@@ -67,13 +74,11 @@ public class Main {
         }
 
         int people = sum/cnt;
-        for(int r=0; r<N; r++) {
-            for(int c=0; c<N; c++) {
-                if(visited[r][c] == num) {
-                    arr[r][c] = people;
-                }
-            }
+        for(int[] now : country) {
+            arr[now[0]][now[1]] = people;
         }
+
+        return cnt > 1;
     }
 
 }
