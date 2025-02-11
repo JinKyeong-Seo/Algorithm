c5848fe6645dd7b6cc81c1c83fe363f45133b6cc
JinKyeong-Seo <103301658+JinKyeong-Seo@users.noreply.github.com>
2025-02-11 11:51:43 +0900

    [Silver II] Title: 나무 자르기, Time: 588 ms, Memory: 175252 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -4,35 +4,44 @@ import java.util.*;
 public class Main {
     static BufferedReader br;
     static StringTokenizer st;
-    static int[] trees;
+    static int[] tree;
+    static int N;
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        trees = new int[N];
-        for(int i=0; i<N; i++) trees[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(trees);
 
-        System.out.println(binarySearch(N, M));
+        st = new StringTokenizer(br.readLine());
+        tree = new int[N];
+        for(int n=0; n<N; n++) {
+            tree[n] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(tree);
+
+        System.out.println(binarySearch(tree[N-1], M)-1);
     }
 
-    public static int binarySearch(int n, int m) {
-        int start = 0;
-        int end = trees[n-1];
-        while(start<=end){
-            int mid = (start+end)/2;
-            long res = 0;
-            for(int i=0; i<n; i++){
-                res += Math.max(trees[i]-mid, 0);
-            }
-            if(res<m){
-                end = mid-1;
+    public static int binarySearch(int end, int target) {
+        int start = 1;
+        int mid;
+        while(start < end) {
+            mid = (start+end)/2;
+            if(calcLength(mid) < target) {
+                end = mid;
             } else {
                 start = mid+1;
             }
         }
-        return start-1;
+        return start;
+    }
+
+    public static long calcLength(int distance) {
+        long sum = 0;
+        for(int n=0; n<N; n++) {
+            if(tree[n] < distance) continue;
+            sum += (tree[n]-distance);
+        }
+        return sum;
     }
 }
