4ec0343714aa5b4ab3098790102cf0b05f7f7279
JinKyeong-Seo <103301658+JinKyeong-Seo@users.noreply.github.com>
2025-02-14 10:14:04 +0900

    [Gold III] Title: 줄 세우기, Time: 388 ms, Memory: 45932 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<Integer>[] adjList;
+    static int[] degree;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        adjList = new ArrayList[N+1];
+        for(int n=1; n<=N; n++) {
+            adjList[n] = new ArrayList<>();
+        }
+
+        degree = new int[N+1];
+        for(int m=0; m<M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            adjList[a].add(b);
+            degree[b]++;
+        }
+
+        topologicalSorting();
+        System.out.println(sb);
+    }
+
+    public static void topologicalSorting() {
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int n=1; n<=N; n++) {
+            if(degree[n]==0) queue.add(n);
+        }
+
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+            sb.append(now).append(" ");
+
+            for(int next : adjList[now]) {
+                degree[next]--;
+                if(degree[next]==0) queue.add(next);
+            }
+        }
+    }
+}
